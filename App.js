71623d6ffddcc3b5462d2eba602234823dd98c4b
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2023-02-25 16:39:42 +0200

    done hw2 stylizationn

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { useCallback } from "react";
+import {
+  Dimensions,
+  ImageBackground,
+  Platform,
+  StatusBar,
+  SafeAreaView,
+} from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { styles } from "./styles";
+
+import LoginScreen from "./Screens/LoginScreen/LoginScreen";
+import RegistrationScreen from "./Screens/RegistrationScreen/RegistrationScreen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  console.log(Platform.OS);
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+        <ImageBackground
+          style={{
+            ...styles.image,
+            width: Dimensions.get("window").width,
+            height: Dimensions.get("window").height,
+          }}
+          source={require("./assets/bg.jpg")}
+        ></ImageBackground>
+        {false ? <RegistrationScreen /> : <LoginScreen />}
+        <StatusBar style="auto" />
+        {/* <StatusBar barStyle={"dark-content"} /> */}
+      </SafeAreaView>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
